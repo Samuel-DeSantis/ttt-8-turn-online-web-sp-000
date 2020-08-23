6f225465db350f46d15449f6443dcb590edaffb8
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-23 05:04:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
 
   validated_input = input_to_index(input)
   if (valid_move?(board, validated_input))
-    move(board, validated_input, value[0])
+    move(board, validated_input)
   else
     until (valid_move?(board, validated_input))
       puts "Please enter 1-9:"

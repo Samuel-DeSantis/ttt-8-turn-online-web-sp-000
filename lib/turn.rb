472a5c782f51836ad6e45e66916e72437930e496
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-23 05:08:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,11 +5,13 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 def input_to_index(input)
   input = input.to_i
   input -= 1
   return input
 end
+
 def position_taken?(board, index)
   if (board[index] === " " || board[index] === "")
     return false
@@ -19,6 +21,7 @@ def position_taken?(board, index)
     return true
   end
 end
+
 def valid_move?(board, index)
   if (index <= 8 && !(position_taken?(board, index)))
     return true
@@ -26,9 +29,11 @@ def valid_move?(board, index)
     return false
   end
 end
+
 def move(board, index, value = "X")
   board[index] = value
 end
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip

86aa3ae5cfaad47c422363d6248fbb6d0326c6ed
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-23 03:52:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,19 +12,24 @@ def input_to_index(input)
   return input_i
 end
 
-def valid_move?(board, index)
+def position_taken?(board, index)
   if (board[index] === " " || board[index] === "")
-    return true
+    return false
   elsif (board[index] === nil)
+    return false
+  elsif (board[index] === "X" || board[index] === "O")
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if (index <= 8 && !(position_taken?(board, index)))
     return true
   else
     return false
   end
 end
 
-def move(board, index, value = "X")
-  board[index] = value
-end
 
 def turn
   board = [" ", " ",  " ",  " ",  " ",  " ",  " ",  " ",  " "]
